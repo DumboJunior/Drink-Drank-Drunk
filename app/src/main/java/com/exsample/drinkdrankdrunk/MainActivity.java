b8d763c2ae5188f1effa4b9a65eccc794c137bae
jensh <jeive17@student.sdu.dk>
2018-03-26 11:43:31 +0200

    wa

--- app/src/main/java/com/exsample/drinkdrankdrunk/MainActivity.java
+++ app/src/main/java/com/exsample/drinkdrankdrunk/MainActivity.java
@@ -1,13 +1,25 @@
 package com.exsample.drinkdrankdrunk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String test = "com.exsample.drinkdrankdrunk.Results";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void change_view(View view){
+        Intent startNewView = new Intent(this, Results.class);
+        TextView text = (TextView) findViewById(R.id.textView2);
+        String newString = text.getText().toString();
+
+        startNewView.putExtra(test,newString);
+        startActivity(startNewView);
     }
 }
