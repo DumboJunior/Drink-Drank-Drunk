a163ca1043bc88ebbc59b22ec9aebc349ca7b602
jensh <jeive17@student.sdu.dk>
2018-03-26 13:54:52 +0200

    wa

--- app/src/main/java/com/exsample/drinkdrankdrunk/MainActivity.java
+++ app/src/main/java/com/exsample/drinkdrankdrunk/MainActivity.java
@@ -6,20 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String test = "com.exsample.drinkdrankdrunk.Results";
+    boolean gender = true;
+    public String pro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onToggleClicked(View view){
+        boolean on = ((ToggleButton) view).isChecked();
+
+        if(!on){
+            gender = true;
+        }else{
+            gender = false;
+        }
+    }
+
     public void change_view(View view){
         Intent startNewView = new Intent(this, Results.class);
-        TextView text = (TextView) findViewById(R.id.textView2);
-        String newString = text.getText().toString();
 
-        startNewView.putExtra(test,newString);
+        EditText vaegt = (EditText) findViewById(R.id.editText);
+        String value = vaegt.getText().toString();
+        int vaegten = Integer.parseInt(value);
+
+        EditText vaegt2 = (EditText) findViewById(R.id.editText2);
+        String value2 = vaegt2.getText().toString();
+        int vaegten2 = Integer.parseInt(value2);
+
+
+        double promile = 0;
+
+        if(gender == true){
+           promile = vaegten2/(vaegten*0.7);
+        }else{
+            promile = vaegten2/(vaegten*0.55);
+        }
+        
+        pro = String.valueOf(promile);
+        startNewView.putExtra("s",pro);
+
         startActivity(startNewView);
+
     }
 }
--- app/src/main/java/com/exsample/drinkdrankdrunk/Results.java
+++ app/src/main/java/com/exsample/drinkdrankdrunk/Results.java
@@ -1,13 +1,22 @@
 package com.exsample.drinkdrankdrunk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
-
+    String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        s = intent.getStringExtra("s");
+        s = "Din promille er: " + s;
+        tv.setText(s);
     }
 }
