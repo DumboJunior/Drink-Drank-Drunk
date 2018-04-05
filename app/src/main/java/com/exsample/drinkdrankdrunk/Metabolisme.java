f5a9a643d5594fbc7ed5b05eae93942d7bdf9efd
DumboJunior <chesb17@student.sdu.dk>
2018-04-05 19:52:46 +0200

    Metabolisme

--- app/src/main/java/com/exsample/drinkdrankdrunk/Metabolisme.java
+++ app/src/main/java/com/exsample/drinkdrankdrunk/Metabolisme.java
@@ -12,7 +12,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 public class Metabolisme extends AppCompatActivity {
     String sex;
     String promil;
-   // LineGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> series;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,28 +20,28 @@ public class Metabolisme extends AppCompatActivity {
         Intent intent = getIntent();
         sex = intent.getStringExtra("sex");
         promil = intent.getStringExtra("promil");
-        /*
+
         double promille;
         promille = Double.parseDouble(promil);
-
+        double gender;
+        gender = Double.parseDouble(sex);
         double y,x; x=0;
-*/
+
 
         TextView promil_tv = (TextView) findViewById(R.id.textView5);
         promil_tv.setText(promil);
 
         TextView sex_tv = (TextView) findViewById(R.id.textView6);
         sex_tv.setText(sex);
-/*
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for(int i=0; i<500;i++){
+        for(int i=0; i<240;i++){
         x= x+0.1;
-        y=-115/(1000*sex)*x+promille;
-        series.appendData(new DataPoint(x,y),true,500);
+        y=-115/(1000 * gender)*x+promille;
+        series.appendData(new DataPoint(x,y),true,200);
         }
         graph.addSeries(series);
-*/
 
 
     }
